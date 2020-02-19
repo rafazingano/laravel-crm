954c607cee8401c25d707ebf3c23f2888d66bfe1
rafazingano <rafazingano@gmail.com>
2020-02-19 17:37:44 -0300

    Update all

--- src/Databases/Migrations/2014_06_27_091705_create_steps_table.php
+++ src/Databases/Migrations/2014_06_27_091705_create_steps_table.php
@@ -21,6 +21,40 @@ class CreateStepsTable extends Migration
             $table->timestamps();
             $table->softDeletes();
         });
+
+        Schema::create('crm_step_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('step_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->foreign('step_id')
+                ->references('id')
+                ->on('crm_steps')
+                ->onDelete('cascade');
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->primary(['step_id', 'user_id']);
+        });
+
+        Schema::create('crm_step_role', function (Blueprint $table) {
+            $table->unsignedBigInteger('step_id');
+            $table->unsignedBigInteger('role_id');
+
+            $table->foreign('step_id')
+                ->references('id')
+                ->on('crm_steps')
+                ->onDelete('cascade');
+
+            $table->foreign('role_id')
+                ->references('id')
+                ->on('entrust_roles')
+                ->onDelete('cascade');
+
+            $table->primary(['step_id', 'role_id']);
+        });
     }
 
     /**
@@ -30,6 +64,8 @@ class CreateStepsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('crm_step_role');
+        Schema::dropIfExists('crm_step_user');
         Schema::dropIfExists('crm_steps');
     }
 }
